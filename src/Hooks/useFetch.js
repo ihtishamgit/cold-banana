d248d7e2325b827081a3097fc640bfd34f074eb9
ihtisham hussain <ihtisham.hussain@starzplayarabia.com>
2024-09-01 14:24:38 +0500

    usefetch hook and error handling

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [url]);
+
+  return { data, error, loading };
+}
--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -1,58 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import useFetch from "../../Hooks/useFetch";
 import { ShimmerFeaturedGallery } from "react-shimmer-effects";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const ProductList = () => {
-  useEffect(() => {
-    getData();
-  }, []);
-  const [data, setData] = useState("");
-  const [loading, setLoading] = useState(false);
-  async function getData() {
-    const url =
-      "https://my-json-server.typicode.com/TomSearle/cb-devtest-api/products";
-    try {
-      setLoading(true);
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-
-      const json = await response.json();
-      setData(json);
-      console.log(json);
-    } catch (error) {
-      console.error(error.message);
-    } finally {
-      setLoading(false);
+  const { data, loading, error } = useFetch(apiUrl);
+  const ProductList = () => {
+    if (error) {
+      return <h1 className="error">Some error occurred. No Data!</h1>;
+    } else {
+      return loading ? (
+        <ShimmerFeaturedGallery row={2} col={2} card frameHeight={600} />
+      ) : (
+        <>
+          <div class="container">
+            <div class="box-container">
+              {data &&
+                data.map((item) =>
+                  item.map(({ image, product_name, price }) => (
+                    <div class="box">
+                      <img src={image} alt={product_name} />
+                      <h3 className="font-weight-700">{product_name}</h3>
+                      <p className="font-weight-700">£{price}</p>
+                    </div>
+                  ))
+                )}
+            </div>
+          </div>
+          <div className="load-more-btn">Load More</div>
+        </>
+      );
     }
-  }
-  console.log(data, "data");
+  };
 
   return (
     <section className="product-list-container">
-      <main className="card-container">
-        {loading ? (
-          <ShimmerFeaturedGallery row={2} col={2} card frameHeight={600} />
-        ) : (
-          <>
-            <div class="container">
-              <div class="box-container">
-                {data &&
-                  data.map((item) =>
-                    item.map(({ image, product_name, price }) => (
-                      <div class="box">
-                        <img src={image} alt={product_name} />
-                        <h3 className="font-weight-700">{product_name}</h3>
-                        <p className="font-weight-700">£{price}</p>
-                      </div>
-                    ))
-                  )}
-              </div>
-            </div>
-            <div className="load-more-btn">Load More</div>
-          </>
-        )}
-      </main>
+      <main className="card-container">{ProductList()}</main>
     </section>
   );
 };
